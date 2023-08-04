a28335072a34ccd20e24a6bb38ad1966c702ea35
mlc <865124665@qq.com>
2023-08-04 15:17:25 +0800

    提交test修改

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,2 +1,18 @@
-package com.example.demo.controller;public class HelloController {
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+
+    @GetMapping()
+    public void test() {
+        System.out.println("Hello World!");
+    }
+
 }
